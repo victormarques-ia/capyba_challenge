11aa636cee645a40bc185eb96808d71ef4d3edfd
Victor Silva <victormarques.ia@gmail.com>
2021-03-04 03:17:20 -0300

    feat: create AuthService

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:capyba_challenge/models/user_model.dart';
+
+import '../models/user_model.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  UserModel _userFromFirebaseUser(User user) {
+    return user != null
+        ? (new UserModel(
+            uid: user.uid,
+            activated: user.emailVerified,
+          ))
+        : null;
+  }
+
+  Stream<UserModel> get user {
+    return _auth.authStateChanges().map(_userFromFirebaseUser);
+  }
+
+  Future registerUser(UserModel userModel) async {
+    try {
+      UserCredential authResult = await _auth.createUserWithEmailAndPassword(
+        email: userModel.email,
+        password: userModel.password,
+      );
+      User user = authResult.user;
+
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signInWithEmailAndPassword(UserModel userModel) async {
+    try {
+      UserCredential authResult = await _auth.signInWithEmailAndPassword(
+        email: userModel.email,
+        password: userModel.password,
+      );
+      User user = authResult.user;
+
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
